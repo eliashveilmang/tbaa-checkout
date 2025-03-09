efee595b8679cfad4a4a58e14da364afbcad4915
elias <eliasimg@MacBookAir.fritz.box>
2025-03-09 19:36:47 +0100

    “Update”

--- public/checkout.js
+++ public/checkout.js
@@ -13,14 +13,11 @@ async function initialize() {
     // Fetch Checkout Session and retrieve the client secret
     const fetchClientSecret = async () => {
       try {
-        const response = await fetch("/create-checkout-session", {
+        const response = await fetch("https://tbaa-ehv-4792f0431457.herokuapp.com/create-checkout-session", {
           method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
+          headers: { "Content-Type": "application/json" },
+        });        
 
-        
         if (!response.ok) {
           const errorText = await response.text();
           console.error(`Error response from server: ${errorText}`);
